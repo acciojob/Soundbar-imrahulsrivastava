363cfe6f26c85344a500b8617b01727a02a6bf07
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-20 20:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-//your JS code here. If required.
+// Write your script here
+
+document.addEventListener("DOMContentLoaded", function () {
+  const buttonsContainer = document.getElementById("buttons");
+
+  const audioFiles = {
+    applause: "sounds/applause.mp3",
+    boo: "sounds/boo.mp3",
+    gasp: "sounds/gasp.mp3",
+    tada: "sounds/tada.mp3",
+    victory: "sounds/victory.mp3",
+    wrong: "sounds/wrong.mp3",
+  };
+
+  // Function to play audio
+  function playAudio(audioSrc) {
+    const audio = new Audio(audioSrc);
+    audio.play();
+  }
+
+  // Event listener for button clicks
+  buttonsContainer.addEventListener("click", function (event) {
+    const target = event.target;
+
+    if (target.classList.contains("btn")) {
+      const soundName = target.textContent.toLowerCase();
+      const audioSrc = audioFiles[soundName];
+
+      if (audioSrc) {
+        playAudio(audioSrc);
+      }
+    } else if (target.classList.contains("stop")) {
+      // Stop all currently playing audio
+      document.querySelectorAll("audio").forEach(function (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      });
+    }
+  });
+});
