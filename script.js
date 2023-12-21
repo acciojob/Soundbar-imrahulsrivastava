e812066b49efe0a42108c0cf04fec69edee944b9
Rahul Kumar Srivastava <124518238+imrahulsrivastava@users.noreply.github.com>
2023-12-21 14:29:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,35 +2,21 @@
 
 document.addEventListener("DOMContentLoaded", function () {
   const buttonsContainer = document.getElementById("buttons");
-
-  const audioFiles = {
-    applause: "sounds/applause.mp3",
-    boo: "sounds/boo.mp3",
-    gasp: "sounds/gasp.mp3",
-    tada: "sounds/tada.mp3",
-    victory: "sounds/victory.mp3",
-    wrong: "sounds/wrong.mp3",
-  };
-
-  // Function to play audio
-  function playAudio(audioSrc) {
-    const audio = new Audio(audioSrc);
-    audio.play();
+  
+  function playAudio(className) {
+    const audio = document.querySelector(`audio.${className}`);
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play();
+    }
   }
 
-  // Event listener for button clicks
   buttonsContainer.addEventListener("click", function (event) {
     const target = event.target;
 
     if (target.classList.contains("btn")) {
-      const soundName = target.textContent.toLowerCase();
-      const audioSrc = audioFiles[soundName];
-
-      if (audioSrc) {
-        playAudio(audioSrc);
-      }
+      playAudio(target.classList[1]); // Assuming the second class is the sound class.
     } else if (target.classList.contains("stop")) {
-      // Stop all currently playing audio
       document.querySelectorAll("audio").forEach(function (audio) {
         audio.pause();
         audio.currentTime = 0;
